3f781b785606efe144cbbee81d8b85ad48a472b4
SergejMasich <sergejmasich74@gmail.com>
2026-01-18 19:16:39 +0000

    Move Section component to src/components

--- src/deploy/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Deploy() {
-      return (
-          <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
-                <h1>Развертывание</h1>
-                      <p>Раздел в разработке.</p>
-                          </div>
-                            );
-                            }
